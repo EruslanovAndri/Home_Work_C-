0dc015e573264899c252b1a8f382b24e5a34eb75
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-15 14:52:34 +0300

    add Max from two didgit

--- HomeWorkLesson001/Program.cs
+++ HomeWorkLesson001/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(num1 > num2)
+{
+    max = num1;
+}
+else
+{
+   max = num2;
+}
+Console.WriteLine($"Первое число =  {num1}");
+Console.WriteLine($"Второе число =  {num2}");
+Console.WriteLine($"Максимальное из двух чисел = {max}");
+
+// Work
