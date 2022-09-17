7490c41112eed7275e59d7b78921ea3866ef80c7
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-17 14:02:50 +0300

    add  Lesson002FillArrayPrintArrayFindIndexPosition/

---
+++ Lesson002FillArrayPrintArrayFindIndexPosition/Program.cs
@@ -0,0 +1,49 @@
+﻿
+Console.Clear();
+int[] array = new int[10];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int possition = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            possition = index;
+            break;
+        }
+        index++;
+    }
+    return possition;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 7);
+Console.WriteLine(pos);
+
