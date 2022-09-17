28ced65e0702eaca882c92007b513f8c94c95239
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-17 13:23:42 +0300

    add max function

---
+++ Lesson002FunctionAndArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int[] arr = {11,22,3,34,54,6,7,8,9};
+void PrintArray(int[] arr)
+{   
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i == 0) Console.Write( "[");
+        if(i < arr.Length - 1)Console.Write(arr[i] + " , ");
+        else Console.Write(arr[i] + "]");
+    }
+}
+int FindMax(int[] arr)
+{   int max = arr[0];
+    int min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+PrintArray(arr);
+int res = FindMax(arr);
+Console.WriteLine();
+Console.WriteLine($"Max = {res}");
