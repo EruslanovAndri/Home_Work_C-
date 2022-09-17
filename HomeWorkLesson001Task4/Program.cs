7ef535abfb08972a5fa3725f7037fc92d86f04d2
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-17 20:02:56 +0300

    удалил лишний цикл

--- HomeWorkLesson001Task4/Program.cs
+++ HomeWorkLesson001Task4/Program.cs
@@ -12,20 +12,18 @@ Console.WriteLine("Введите второе число:");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число:");
 int c = Convert.ToInt32(Console.ReadLine());
-int size = 3;
 int i = 0;
 int max = a;
 
-while (i < size)
+
+if (b >= max)
 {
-    if (b >= max)
-    {
-        max = b;
-    }
-    if (c >= max)
-    {
-        max = c;
-    }
-    i++;
+    max = b;
 }
+if (c >= max)
+{
+    max = c;
+}
+i++;
+
 Console.WriteLine(max);
