53cb9554a4a2b1436edbaa10608a75b49453481a
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-20 17:45:30 +0300

    add if cycle

---
+++ Lesson002Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿//Task13 Напишите программу, которая сообщает пользователю к какой возрасной группе он относится.
+
+Console.Clear();
+Console.Write("Напишите пожалуйста  ваш возраст: ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(age);
+int young = 10;
+int teenager = 20;
+int adult = 45;
+int old = 100;
+if (age <= young) Console.WriteLine("Вы еще ребенок.");
+else if (age <= teenager) Console.WriteLine("Вы подросток.");
+else if (age <= adult) Console.WriteLine("Вы взрослый.");
+else if (age <= old) Console.WriteLine("Вы пенсионер.");
+
+// Work
