c082c1c6988b6becadf1fb6b91211f8945e5fd54
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-20 13:12:58 +0300

    Task 11 удаления второй цифры из трехзначного числа

---
+++ Lesson002Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿//Task11 Написать программу , которая генерирует трехзначное число и удаляет вторую цифру.
+
+Console.Clear();
+int num = new Random().Next(100,1000);
+Console.WriteLine($"The random number is {num}");
+int firstDidgit = num / 100;
+Console.WriteLine($"The first didgit is {firstDidgit}");
+int secondDidgit = num / 10 % 10;
+Console.WriteLine($"The second didgit is {secondDidgit}");
+int thirdDidgit = num % 10;
+Console.WriteLine($"The third didgit is {thirdDidgit}");
+Console.WriteLine($"Result = {firstDidgit}{thirdDidgit}");
+
+// work
+
+
