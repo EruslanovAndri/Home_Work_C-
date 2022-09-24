a0686e203bf8ac8fb583c326f4b9c84127f3e95f
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-24 12:40:07 +0300

    add cycle for example

---
+++ Lesson003CycleFor/Program.cs
@@ -0,0 +1,11 @@
+﻿// Таблица умножения цикл в цикле.
+Console.Clear();
+
+for (int i = 1; i <=10; i++)
+{
+    for (int j = 1; j <=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
