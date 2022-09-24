367551ab3b8c5a00185be02c14f12b8059933702
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-24 21:16:21 +0300

    add Lesson003Task18

---
+++ Lesson003Task18/Program.cs
@@ -0,0 +1,12 @@
+﻿// task 18 Пр-ма по заданному четверти показывает диапазон возможных точек в этой четверти.
+
+Console.Clear();
+Console.WriteLine("Ввыдите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1) Console.WriteLine("X > 0 and Y > 0");
+if (num == 2) Console.WriteLine("X < 0 and Y > 0");
+if (num == 3) Console.WriteLine("X < 0 and Y < 0");
+if (num == 4) Console.WriteLine("X > 0 and Y < 0");
+if(num > 4)Console.WriteLine("Error");
+// work
