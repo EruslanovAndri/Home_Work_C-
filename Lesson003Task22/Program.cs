e144e88cf122b56c5fbcb668e08d7cd875c9774a
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-25 12:44:40 +0300

    add Lesson003Task22 поиск квадрата числа от 1 до N

---
+++ Lesson003Task22/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task22 Пр-ма принимает число N  и выводит квадратов чисел от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void FindSquareNumber(int num)
+{
+    int res = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        res = i*i;
+        Console.WriteLine($"{i} X {i} = {res}");     
+    }
+}
+FindSquareNumber(num);
+
