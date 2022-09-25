d07f44cb467ded3b4d041ec5f1738a1c66449550
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-25 12:55:40 +0300

    add Home work tsk 22

---
+++ HomeWorkLesson003Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task 22 Написать пр-му , которая принимает число N и выдает куб чисел от 1 д N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void GetCubeOfTheNumber(int num)
+{
+    int res = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        res = i*i*i;
+        Console.WriteLine($"{i} = {res}");
+    }
+}
+GetCubeOfTheNumber(num);
+
+// work 
