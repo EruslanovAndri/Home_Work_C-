871f3b8b159a5b458ee915b9cc614db80d4869b6
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-28 15:46:31 +0300

    add task 27 сумма цифр в числе

---
+++ HomeWorkLesson004Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+while (num > 0)
+{
+    res = res + num % 10;
+    num = num / 10;
+}
+Console.WriteLine(res);
+// work
