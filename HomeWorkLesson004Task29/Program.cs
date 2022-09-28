53a9dcf4487c2f986566fddf4330a69af61cd46f
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-28 16:05:55 +0300

    add task 29 creat and print array

---
+++ HomeWorkLesson004Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+//массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int[] array = new int[8];
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    var rnd = new Random().Next(1,100);
+    array[i] = rnd;
+    if(i == 0 )Console.Write("[ ");
+    if(i < array.Length - 1) Console.Write(array[i] + ",");
+    else Console.Write( array[i] + " ]");
+}
+}
+PrintArray(array);
+// Work
