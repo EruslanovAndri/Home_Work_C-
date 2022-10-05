79d933569fa455b507b4906d312efcd9886fb63f
EruslanovAndrei <eruslanov84as@gmail.com>
2022-10-05 09:19:13 +0300

    Lesson006Task42 перевод десятичных чисел в двоичную систему

---
+++ Lesson006Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// **Задача 42:** Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+// 2 -> 10
+
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+string y ="";
+
+while (x > 0)
+{
+    y  = Convert.ToString(x % 2) + y;
+    //Console.WriteLine(y);
+    x /= 2;
+}
+Console.WriteLine(y);
