7bfa8b675998015d19ab06b32b80b9fff90a2759
EruslanovAndrei <eruslanov84as@gmail.com>
2022-10-10 17:12:25 +0300

    HomeWorkLesson007Task50

---
+++ HomeWorkLesson007Task50/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
+// двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+Console.Clear();
+
+Console.WriteLine("Введите номер строки: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] martix = new int[4, 4];
+
+void FillArrayMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            var rnd = new Random().Next(1, 10);
+            martix[i, j] = rnd;
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindExactElement(int[,] matrix, int row, int column)
+{
+    int element = 0;
+    for (int i = 0; i < martix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (row == i && column == j)
+            {
+                element = martix[i,j];
+            } 
+        }
+    }
+    return element;
+}
+
+FillArrayMatrix(martix);
+PrintMatrix(martix);
+int findElement = FindExactElement(martix,row,column);
+Console.WriteLine(findElement);
+
+
+// work
+
