a09dc80fd17b99ddbdf6d43e6a8004bbe8cba2b1
EruslanovAndrei <eruslanov84as@gmail.com>
2022-10-10 10:50:14 +0300

    Lesson008Task53ChangeRows

---
+++ Lesson008Task53ChangeRows/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+Console.Clear();
+Console.WriteLine("Введите кол-во строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] FillArrayMatrix(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[ ");
+            if (j < matrix.GetLength(1) - 1) Console.Write(matrix[i, j] + "|");
+            else Console.Write(matrix[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeRows(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp; 
+    }
+}
+
+int[,] matrix = FillArrayMatrix(rows, columns);
+PrintMatrix(matrix);
+ChangeRows(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+
+// work
