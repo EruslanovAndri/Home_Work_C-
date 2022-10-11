c94189781c4aa9bfc89d45ab90e7b6fc61aa2e81
EruslanovAndrei <eruslanov84as@gmail.com>
2022-10-11 14:42:11 +0300

    HomeWorkLesson006Task44/

---
+++ HomeWorkLesson006Task44/Program.cs
@@ -0,0 +1,22 @@
+﻿// **адача 44:** Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Clear();
+Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+int fib0 = 0;
+int fib1 = 1;
+
+for (int i = 2; i <= n; i++)
+{
+   
+
+}
+Console.WriteLine(fib0);
+Console.WriteLine(fib1);
+
+
+
